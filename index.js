b801399fa08f3c0dde1f49d4ee0299f32c1ca9c0
chibiProgrammer <nakamura_m@uuum.jp>
2020-04-22 20:11:57 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// canvasの準備
+var canvas = document.getElementById('canvas');
+var canvasWidth = 480;
+var canvasHeight = 270;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+var ctx = canvas.getContext('2d');
+
+var willShowDLLink = false;
+
+// Canvas上に背景画像を表示
+var img = new Image();
+img.src = 'img/background5.png';
+img.onload = function() {
+    ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+}
+
+/* 画像を読み込んで画像を表示 */
+
+var file1 = document.getElementById('file1');
+var file2 = document.getElementById('file2');
+var uploadImgSrc;
+
+// ファイルが指定された時にloadLocalImage()を実行
+// (dx, dy) には画像を配置する中心点を渡す
+file1.addEventListener('change', function(ev){loadLocalImage(ev, (((canvasWidth / 4) * 1) - 10), (canvasHeight/2 - 10))}, true);
+file2.addEventListener('change', function(ev){loadLocalImage(ev, (((canvasWidth / 4) * 3) + 10), (canvasHeight/2 - 10))}, true);
+
+
+function loadLocalImage(e, dx, dy) {
+
+    var fileData = e.target.files[0];
+
+    // 画像ファイル以外は処理を止める
+    if(!fileData.type.match('image.*')) {
+        alert('画像を選択してください');
+        return;
+    }
+
+    // ファイル読み込み
+    var reader = new FileReader();
+    reader.onload = function() {
+        // Canvas上に表示する
+        uploadImgSrc = reader.result;
+        canvasDraw(dx, dy);
+    }
+    // ファイル読み込みを実行
+    reader.readAsDataURL(fileData);
+}
+
+
+// Canvas上に画像を表示する
+function canvasDraw(dx, dy) {
+
+    // Canvas上に画像を表示
+    var img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = uploadImgSrc;
+    img.onload = function() {
+        
+        var maxWidth = 150;
+        var maxHeight  = 150;
+
+        var isYokonaga = this.width > this.height;
+
+        if (isYokonaga) {
+
+            console.log("横長画像");
+            var width = maxWidth;
+            var height = maxWidth * (this.height / this.width);
+            
+        } else {
+
+            console.log("縦長画像");
+            var width = maxHeight * (this.width / this.height);
+            var height = maxHeight;
+
+        }
+
+        dx = dx - width/2;
+        dy = dy - height/2;
+
+        ctx.drawImage(img, dx, dy, width, height );
+
+
+        if (willShowDLLink) {
+
+            // canvasを画像に変換
+            var data = canvas.toDataURL();
+
+            // DLリンクを表示
+            $('#result').attr("href", data);
+            $("#result").attr("download", "「いいねvsRT画像」メーカー.png");
+            $('#result').text("画像をダウンロード");
+
+        } else {
+
+            willShowDLLink = true;
+
+        }
+    }
+
+}
---
+++ js/index.js
@@ -0,0 +1,215 @@
+// 1: 回数 2: 秒数　
+
+var easyMenu = [
+    ["もも上げ運動", 1],
+    ["エア自電車", 2],
+    ["ツイストクランチ", 1],
+    ["ランジ", 1],
+];
+
+var normalMenu = [
+    ["もも上げ運動", 1],
+    ["エア自電車", 2],
+    ["ツイストクランチ", 1],
+    ["ランジ", 1],
+    ["スクワット", 1],
+    ["腹筋", 1],
+    ["腕立て伏せ", 1],
+];
+
+var difficultMenu = [
+    ["もも上げ運動", 1],
+    ["エア自電車", 2],
+    ["ツイストクランチ", 1],
+    ["ランジ", 1],
+    ["スクワット", 1],
+    ["腹筋", 1],
+    ["腕立て伏せ", 1],
+    ["プランク", 2],
+    ["レッグレイズ", 1]
+];
+
+
+function twitText(button) 
+{
+    var id_value = button.id
+    url = document.location.href;
+    switch(id_value){
+        case 'easy':
+            var tweetText = 'https://twitter.com/intent/tweet?text='+
+            encodeURIComponent(
+                "今日する運動\n" + 
+                easyMotion(easyMenu) +
+                '#うんどうしたったー'
+            );
+        break;
+
+        case 'normal':
+            var tweetText = 'https://twitter.com/intent/tweet?text='+
+            encodeURIComponent(
+                "今日する運動\n" + 
+                normalMotion(normalMenu) +
+                '#うんどうしたったー'
+            );
+        break;
+
+        case 'difficult':
+            var tweetText = 'https://twitter.com/intent/tweet?text='+
+            encodeURIComponent(
+                "今日する運動\n" + 
+                difficultMotion(difficultMenu) +
+                '#うんどうしたったー'
+            );
+        break;
+
+        default:
+        break;
+    }
+    window.open(tweetText,"_blank","width=600,height=300");
+};
+
+function easyMotion(menu)
+{
+
+    var dailyEasyMenu = new Array(menu.length);
+    var daily = [];
+
+    var dailyMenu = [];
+    var dailyMenuNum = [];
+
+    for (i=0; i<dailyEasyMenu.length; i++){
+        dailyEasyMenu[i] = new Array(2);
+    }
+
+    var dailyEasyMenuNum = getRandomInt(1, menu.length);
+
+    for(var i=0;i<dailyEasyMenuNum;i++){
+        dailyEasyMenu[i][0] = menu[getRandomInt(0, menu.length-1)]
+        daily[i] = dailyEasyMenu[i][0][0]
+    }
+
+    dailyMenu = daily.filter(function(x, i, self){
+        return self.indexOf(x) === i;
+    });
+
+    for(var i=0;i<dailyMenu.length;i++){
+        for(var j=0;j<menu.length;j++){
+            if(dailyMenu[i] == menu[j][0]){
+                dailyMenuNum[i] = getEasyExercisesNumber(menu[j][1]);
+            }
+        }
+    }
+    return getTweet(dailyMenu, dailyMenuNum);
+}
+
+function normalMotion(menu)
+{
+
+    var dailyEasyMenu = new Array(menu.length);
+    var daily = [];
+
+    var dailyMenu = [];
+    var dailyMenuNum = [];
+
+    for (i=0; i<dailyEasyMenu.length; i++){
+        dailyEasyMenu[i] = new Array(2);
+    }
+
+    var dailyEasyMenuNum = getRandomInt(1, menu.length);
+
+    for(var i=0;i<dailyEasyMenuNum;i++){
+        dailyEasyMenu[i][0] = menu[getRandomInt(0, menu.length-1)]
+        daily[i] = dailyEasyMenu[i][0][0]
+    }
+
+    dailyMenu = daily.filter(function(x, i, self){
+        return self.indexOf(x) === i;
+    });
+
+    for(var i=0;i<dailyMenu.length;i++){
+        for(var j=0;j<menu.length;j++){
+            if(dailyMenu[i] == menu[j][0]){
+                dailyMenuNum[i] = getNormalExercisesNumber(menu[j][1]);
+            }
+        }
+    }
+    return getTweet(dailyMenu, dailyMenuNum);
+}
+
+function difficultMotion(menu)
+{
+
+    var dailyEasyMenu = new Array(menu.length);
+    var daily = [];
+
+    var dailyMenu = [];
+    var dailyMenuNum = [];
+
+    for (i=0; i<dailyEasyMenu.length; i++){
+        dailyEasyMenu[i] = new Array(2);
+    }
+
+    var dailyEasyMenuNum = getRandomInt(1, menu.length);
+
+    for(var i=0;i<dailyEasyMenuNum;i++){
+        dailyEasyMenu[i][0] = menu[getRandomInt(0, menu.length-1)]
+        daily[i] = dailyEasyMenu[i][0][0]
+    }
+
+    dailyMenu = daily.filter(function(x, i, self){
+        return self.indexOf(x) === i;
+    });
+
+    for(var i=0;i<dailyMenu.length;i++){
+        for(var j=0;j<menu.length;j++){
+            if(dailyMenu[i] == menu[j][0]){
+                dailyMenuNum[i] = getDifficultExercisesNumber(menu[j][1]);
+            }
+        }
+    }
+    return getTweet(dailyMenu, dailyMenuNum);
+}
+
+function getTweet(menu, num){
+    var tweetText = "";
+
+    for(i=0;i<menu.length;i++){
+        tweetText += (menu[i] + " : " + num[i] + "\n");
+    }
+
+    return tweetText;
+}
+
+function getEasyExercisesNumber(num){
+    // 1: 回数 2: 秒数　
+    if(num == 1){
+        return (parseInt(Math.random() * 3) + 1) * 10 + "回"
+    }
+    if(num == 2){
+        return (parseInt(Math.random() * 2) + 1) * 60 + "秒"
+    }
+}
+
+function getNormalExercisesNumber(num){
+    // 1: 回数 2: 秒数　
+    if(num == 1){
+        return (parseInt(Math.random() * 5) + 2) * 10 + "回"
+    }
+    if(num == 2){
+        return (parseInt(Math.random() * 4) + 2) * 60 + "秒"
+    }
+}
+
+function getDifficultExercisesNumber(num){
+    // 1: 回数 2: 秒数　
+    if(num == 1){
+        return (parseInt(Math.random() * 10) + 3) * 10 + "回"
+    }
+    if(num == 2){
+        return (parseInt(Math.random() * 10) + 3) * 60 + "秒"
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor( Math.random() * (max + 1 - min) ) + min ;
+}
